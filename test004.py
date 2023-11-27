8865269e7dd8714441c71fb47384cdd5dcfb829b
omargtx2 <omargtx2@yahoo.es>
2023-11-27 16:37:40 -0500

    #test004.py local server change 4 feature_branch lu

--- test004.py
+++ test004.py
@@ -7,4 +7,5 @@
 #test004.py local server change 2
 #test004.py local server change 3
 
-#test004.py local server change 4 feature_branch lu
+#test004.py local server change 1 feature_branch lu
+#test004.py local server change 2 feature_branch lu
